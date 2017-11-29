1f4a3853e408a58e43d99e3a381609b6fd60da21
Makkrrssou <max.oliny@gmail.com>
2017-11-29 17:48:51 -0400

    resolution probleme sprite et jeu complet

--- labyrinthe_4.py
+++ labyrinthe_4.py
@@ -62,9 +62,8 @@ class Mur(pygame.sprite.Sprite):
                 
 
 
-class Depart(Elem):
+class Depart():
         def __init__(self):
-                Elem.__init__(self)
                 self.x=0
                 self.y=0
                 image_depart="images/depart.png"
@@ -72,38 +71,31 @@ class Depart(Elem):
                 
 
 
-class Arrivee(Elem):
-        def __init__(self):
-                Elem.__init__(self)
-                self.x=0
-                self.y=0
+class Arrivee(pygame.sprite.Sprite):
+        def __init__(self,o,liste):
+                pygame.sprite.Sprite.__init__(self)
                 image_arrivee="images/arrivee.png"
                 self.image=pygame.image.load(image_arrivee).convert()
-                self.image_rect=self.image.get_rect()
-                arriveSprite=list()
+                self.rect=self.image.get_rect()
+                self.rect.x,self.rect.y=liste[o]
 
-        def arriveeSprite(self):
 
-            for i in Elem.mur:
-                
-                self.image_rect.x=i[0]
-                self.image_rect.y=i[1]
-                arriveeSprite.append(i)
-                
-            return arriveeSprite
             
 
 
-class Objets(Elem):
-        def __init__(self):
-                Elem.__init__(self)
+class Objets(pygame.sprite.Sprite):
+        def __init__(self,liste):
+                pygame.sprite.Sprite.__init__(self)
                 self.x=0
                 self.y=0
-                image_obj="images/objets.png"
-                self.image1=pygame.image.load(image_obj).convert_alpha()
-                self.image2=pygame.image.load(image_obj).convert_alpha()
-                self.image3=pygame.image.load(image_obj).convert_alpha()
-                self.list_image=[self.image1,self.image2,self.image3]
+                self.list_image=["images/objets.png","images/objets.png","images/objets.png"]
+                for i in self.list_image:
+                        self.image=pygame.image.load(i).convert_alpha()
+                        self.rect=self.image.get_rect()
+                        self.rect.x,self.rect.y=random.choice(liste)
+                        
+                
+
 
 class Perso(pygame.sprite.Sprite):
         def __init__(self,image):
@@ -120,23 +112,40 @@ class Perso(pygame.sprite.Sprite):
                         if self.y<15:
                                 self.y+=1
                                 self.rect.y=self.y* 30
+                                if pygame.sprite.spritecollide(self,murGroupSprite,False):
+                                        self.y-=1
+                                        self.rect.y=self.y* 30
+                                        print('collision en bas')
+                                        
 
                 if direction=='haut':
                         if self.y>0:
                                 self.y-=1
                                 self.rect.y=self.y* 30
+                                if pygame.sprite.spritecollide(self,murGroupSprite,False):
+                                        self.y+=1
+                                        self.rect.y=self.y* 30
+                                        print('collision en haut')
 
                                 
                 if direction=='droite':
-                    if self.x<=15:
+                    if self.x<15:
                             self.x+=1
                             self.rect.x=self.x* 30
+                            if pygame.sprite.spritecollide(self,murGroupSprite,False):
+                                        self.x-=1
+                                        self.rect.x=self.x* 30
+                                        print('collision à droite')
 
                             
                 if direction=='gauche':
                         if self.x>0:
                                 self.x-=1
                                 self.rect.x=self.x* 30
+                                if pygame.sprite.spritecollide(self,murGroupSprite,False):
+                                        self.x+=1
+                                        self.rect.x=self.x* 30
+                                        print('collision à gauche')
                 return self.rect.x,self.rect.y
             
         
@@ -153,9 +162,6 @@ text="laby.txt"
 elem=Elem()
 elem.extract_elem(text)
 depart=Depart()
-##mur=Mur()
-arrivee=Arrivee()
-obj=Objets()
 
 img_macgyver="images/macgyver.png"
 macgyver=Perso(img_macgyver)
@@ -163,37 +169,45 @@ macgyver=Perso(img_macgyver)
 fond=pygame.image.load(image_fond).convert()
 
 murSprite=list()
+arriveeSprite=list()
 
 for i,o in enumerate(elem.mur):
         murSprite.append(Mur(i,elem.mur))
 
+for i,o in enumerate(elem.arrivee):
+        arriveeSprite.append(Arrivee(i,elem.arrivee))
 
 murGroupSprite=pygame.sprite.Group(murSprite)
+macgyverSprite=pygame.sprite.Group(macgyver)
+arriveeGroupSprite=pygame.sprite.Group(arriveeSprite)
 
 
 
 
 fenetre.blit(fond,(0,0))
-position_obj=[]
-for o in obj.list_image:
-        pos=random.choice(elem.couloir)
-        fenetre.blit(o,pos)
-        position_obj.append(pos)
+position_objSprite=[]
+for o in Objets(elem.couloir).list_image:
+        position_objSprite.append(Objets(elem.couloir))
+
+objetsSprite=pygame.sprite.Group(position_objSprite)
+cpt_obj=0
 pygame.display.flip()
 
-while 1:
+jouer=True
+while jouer:
 
         for elmt in elem.depart:
             fenetre.blit(depart.image,elmt)
-        
 
-##        for elmt in elem.mur:
-##            fenetre.blit(mur.image,elmt)
+        murGroupSprite.draw(fenetre)
 
-        for elmt in elem.arrivee:
-            fenetre.blit(arrivee.image,elmt)
+        arriveeGroupSprite.draw(fenetre)
+
+        objetsSprite.draw(fenetre)
         
-        fenetre.blit(macgyver.image,(macgyver.rect.x,macgyver.rect.y))
+        macgyverSprite.draw(fenetre)
+
+        
         pygame.display.flip()
         for event in pygame.event.get():
                 if event.type == KEYDOWN:
@@ -208,21 +222,31 @@ while 1:
 
                         elif event.key == K_DOWN:
                             macgyver.deplacer('bas')
+
+        if pygame.sprite.spritecollide(macgyver,objetsSprite,True):
+                cpt_obj+=1
+                print('Récupération objets')
+
+        if pygame.sprite.spritecollide(macgyver,arriveeGroupSprite,False):
+                if cpt_obj==len(position_objSprite):
+                        print('Gagné')
+                        jouer=False
+                else :
+                        print('perdu')
+                        jouer=False
+                
            
         fenetre.blit(fond,(0,0))            
 
         for elmt in elem.depart:
             fenetre.blit(depart.image,elmt)
-        
 
-##       for elmt in elem.mur:
-            ##fenetre.blit(mur.image,elmt)
-
-        for elmt in elem.arrivee:
-            fenetre.blit(arrivee.image,elmt)
         
-        for i,o in enumerate(obj.list_image):
-                fenetre.blit(o,position_obj[i])
+        murGroupSprite.draw(fenetre)
+
+        arriveeGroupSprite.draw(fenetre)
+
+        objetsSprite.draw(fenetre)
                 
-        fenetre.blit(macgyver.image,(macgyver.rect.x,macgyver.rect.y))
+        macgyverSprite.draw(fenetre)
         pygame.display.flip()
